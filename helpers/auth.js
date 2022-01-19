0d71b57f159a50deadf6ca6b98adf61f7d3db23a
martin kyalo <martinkkyalo10@gmail.com>
2022-01-19 21:09:10 +0300

    nextauth functionality implimented at catch all api route and configured

--- helpers/auth.js
+++ helpers/auth.js
@@ -1,6 +1,13 @@
-import { hash } from "bcryptjs";
+import { hash, compare } from "bcryptjs";
 
 export async function hashPassword(password) {
   const hashedPassword = await hash(password, 12);
   return hashedPassword;
+}
+
+// find if the hashed passoword is equal to the plain text password
+
+export async function verifyPassword(password, hashedPassword) {
+  const isValid = await compare(password, hashedPassword);
+  return isValid;
 }
---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,51 @@
+import NextAuth from "next-auth";
+import Providers from "next-auth/providers";
+import { verifyPassword } from "../../../helpers/auth";
+import { connectToDatabase } from "../../../helpers/db";
+
+// next auth must be exported as a function
+export default NextAuth({
+  // ensure JWT is generated
+  session: {
+    jwt: true,
+  },
+
+  // setup auth providers
+  Providers: [
+    // config providers to bring your own credentials, select credentials provider
+    Providers.Credentials({
+      async authorize(credentials) {
+        //   connect to database
+        const client = await connectToDatabase();
+
+        // provide auth login
+        const usersCollection = client.db().collection("users");
+        const user = await usersCollection.findOne({
+          email: credentials.email,
+        });
+
+        // not user found
+        if (!user) {
+          client.close();
+          throw new Error("No user found!");
+        }
+
+        // find out if password is correct
+        const isValid = await verifyPassword(
+          credentials.password,
+          user.password
+        );
+
+        // if not valid
+        if (!isValid) {
+          client.close();
+          throw new Error("Could not log you in!");
+        }
+
+        client.close();
+        // now the user is loged in. return an object (userinfo) that must be returned with JWT to client
+        return { email: user.email };
+      },
+    }),
+  ],
+});
